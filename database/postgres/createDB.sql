8c23ceac9d324391f598a6a8a3e0281fd9659be4
wdevarshi <devarshi.waghela@thecarousell.com>
2024-05-11 16:31:27 +0800

    adding commands for setting up db

--- database/postgres/createDB.sql
+++ database/postgres/createDB.sql
@@ -1,3 +1,31 @@
-CREATE ROLE internalTransferSystemUser login SUPERUSER PASSWORD 'mypwd';
-ALTER USER internalTransferSystemUser with CEATEDB CREATEROLE;
-CREATE DATABASE internaltransferssystem OWNER internaltransfersystemuser;
+-- psql postgres
+CREATE
+DATABASE internaltransferssystem;
+-- \c internaltransferssystem
+CREATE TABLE "account"
+(
+    "id"            uuid                     NOT NULL PRIMARY KEY,
+    "balance"       varchar(50)              NOT NULL,
+    "time_created"  timestamp with time zone NOT NULL,
+    "last_modified" timestamp with time zone NOT NULL
+);
+
+create type trx_status as enum (
+  'init',
+  'success',
+  'failed'
+);
+
+CREATE TABLE "trx_ledger"
+(
+    "id"                     uuid                     NOT NULL PRIMARY KEY,
+    "source_account_id"      uuid                     NOT NULL,
+    "destinatio_account_idn" uuid                     NOT NULL,
+    "amount"                 DOUBLE PRECISION         NOT NULL,
+    "status"                 trx_status               NOT NULL,
+    "time_created"           timestamp with time zone NOT NULL,
+    "last_modified"          timestamp with time zone NOT NULL
+);
+
+CREATE INDEX "source_account_idx" on "trx_ledger" ("source_account_id");
+CREATE INDEX "destinatio_account_idnx" on "trx_ledger" ("destinatio_account_idn");
